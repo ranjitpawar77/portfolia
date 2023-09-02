30c41f827f425b97d859d5803212b5369e2d3e2d
dada <omkardhapte99@gmail.com>
2023-09-02 12:40:32 +0530

    strutre add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SobreComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+  data = {
+    name: "Aladeojebi opeyemi",
+    jobtitle: "Product Designer",
+    describe: "I've helped business owners create exceptional experiences for users of Android, iOS and the Web."
+  }
+
+}
